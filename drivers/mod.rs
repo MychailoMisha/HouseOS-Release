636fe4dba6443842b15475eddf59b1c6f44c27ba
MychailoMisha <mametovmychailo@gmail.com>
2026-06-21 17:55:23 +0300

    Add files via upload

---
+++ drivers/mod.rs
@@ -0,0 +1,36 @@
+#![allow(dead_code)]
+
+pub mod port_io;
+pub mod pci;
+pub mod usb;
+pub mod net;
+pub mod battery;
+pub mod storage {
+    pub mod ata_pio;
+    pub mod exfat;
+    pub mod ntfs;
+}
+pub mod audio {
+    pub mod pc_speaker;
+}
+
+pub use battery::init as battery_init;
+
+pub mod video {
+    pub mod vesa_lfb;
+}
+pub mod input {
+    pub mod ps2_controller;
+    pub mod keyboard_ps2;
+    pub mod mouse_ps2;
+    pub mod mouse_cursor;
+}
+pub mod interrupts {
+    pub mod pic8259;
+}
+pub mod timer {
+    pub mod pit8253;
+}
+pub mod serial {
+    pub mod uart16550;
+}
